d9175ef73e454634300fd4efd367cab213e95092
huyenminnn <huyenttm.13@gmail.com>
2019-05-07 18:50:44 +0700

    html-huyen

---
+++ js/js.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+	$("#btn-search").click(function(){
+		$("#search").css('margin-left','5%')
+		$("#search").css('margin-top','10px')
+		$("#logo").css('float','left')
+		$("#search").css('float','left')
+	})
+})
