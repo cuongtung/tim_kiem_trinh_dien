345d22b339d21686fbab10852cfba880237f0ae2
huyenminnn <huyenttm.13@gmail.com>
2019-05-14 19:22:47 +0700

    cmt2

--- controller.py
+++ controller.py
@@ -1,14 +1,19 @@
 # from flask import Flask
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for, request
 app = Flask(__name__)
 
-@app.route("/search")
+@app.route("/")
 def main():
-  return render_template("index.html")
+	return render_template("index.html")
 
-@app.route("/result")
-def result():
-  return render_template("result.html")
+@app.route("/search", methods=['GET'])
+def search():
+	user = request.args.get('input')
+  	return redirect(url_for('result',input = user))
+
+@app.route("/result/<input>")
+def result(input):
+  return render_template("result.html", input=input)
 
 if __name__ == "__main__":
   app.run()
