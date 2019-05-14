84fefc3d558ccb8984d051a513252d72f04d39db
huyenminnn <huyenttm.13@gmail.com>
2019-05-14 18:08:33 +0700

    add

--- controller.py
+++ controller.py
@@ -2,9 +2,13 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/search")
 def main():
   return render_template("index.html")
+
+@app.route("/result")
+def result():
+  return render_template("result.html")
 
 if __name__ == "__main__":
   app.run()
