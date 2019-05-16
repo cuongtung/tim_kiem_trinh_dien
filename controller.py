c1ef0990c0fb752b2dd9f3dbbc039d7d93a3919b
anhnguyen97 <anh.nguyentrung13@gmail.com>
2019-05-17 00:06:14 +0700

    display result in view

--- controller.py
+++ controller.py
@@ -1,27 +1,27 @@
 # from flask import Flask
-import requests
 import json
-from flask import Flask, render_template, redirect, url_for, request
+import requests
+from flask import Flask, render_template, request, redirect, url_for
 from urllib.parse import quote
+
 app = Flask(__name__)
+
 
 @app.route("/")
 def main():
-	return render_template("index.html")
+    return render_template("index.html")
+
 
 @app.route("/search", methods=['GET'])
 def search():
-	input = request.args.get('input')
-	input = "title:"+input
-	query = quote(input)
-	api = "http://localhost:8983/solr/it4853-1/select?q=" + input
-	r = requests.get(api)
-	result = r.json()
-	return render_template("result.html", result=result)
-
-# @app.route("/result/<input>")
-# def result(input):
-#   return render_template("result.html", input=input)
+    input = request.args.get('input')
+    input = "title:" + input
+    query = quote(input)
+    api = "http://localhost:8983/solr/it4853-1/select?q=" + query
+    r = requests.get(api)
+    result = r.json()
+    # result = json.loads(result)
+    return render_template("result.html", result=result)
 
 if __name__ == "__main__":
-  app.run()
+    app.run()
