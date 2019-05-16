f6d4a0b20e2031759465b112fc1de54970977d15
huyenminnn <huyenttm.13@gmail.com>
2019-05-16 15:37:20 +0700

    update

--- controller.py
+++ controller.py
@@ -1,32 +1,27 @@
 # from flask import Flask
 import requests
-from urllib.parse import quote
-
+import json
 from flask import Flask, render_template, redirect, url_for, request
+from urllib.parse import quote
 app = Flask(__name__)
 
 @app.route("/")
 def main():
-
 	return render_template("index.html")
 
 @app.route("/search", methods=['GET'])
 def search():
-    input = request.args.get('input')
-    input = quote(input)
-    api = "http://localhost:8983/solr/it4853-1/select?q=title"+input
-    r = requests.get(api)
-    r = r.json()
-    for e in r:
-        e{'title'}
-        e{'content'}
-    print(r.json)
+	input = request.args.get('input')
+	input = "title:"+input
+	query = quote(input)
+	api = "http://localhost:8983/solr/it4853-1/select?q=" + input
+	r = requests.get(api)
+	result = r.json()
+	return render_template("result.html", result=result)
 
-    return redirect(url_for('result', input = r.json)
-
-@app.route("/result/<input>")
-def result(input):
-  return render_template("result.html", input=input)
+# @app.route("/result/<input>")
+# def result(input):
+#   return render_template("result.html", input=input)
 
 if __name__ == "__main__":
   app.run()
